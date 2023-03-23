ec08a8878ed34dfb74ca28be11c9685534876bd6
RABEH Naji <rabeh.naji@gmail.com>
2023-03-23 09:34:26 -0700

    7. I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,6 +9,7 @@ int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 
 
 #endif
